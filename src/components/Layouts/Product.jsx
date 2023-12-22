d7fb0c6e477b5ffd88d23bd20372ad02344c26b0
mdrayhan124 <ranjerrayhan124@gmail.com>
2023-12-23 02:26:37 +0600

    new

--- src/components/Layouts/Product.jsx
+++ src/components/Layouts/Product.jsx
@@ -7,27 +7,25 @@ import { Link } from 'react-router-dom';
 
 const Product = ({ className,showBadge,productImg,productName,productPrice ,badgeName,link}) => {
   return (
-    <Link to={link} className={`${className} font-dm relative overflow-hidden group-[]:hover:scale-100`}>
-      <Img src={productImg} className={"w-[370px] h-[360px]"} />
-      <Flex className={"justify-between mt-5"}>
-        <h6 className='text-[#262626] font-bold text-xl'>{productName}</h6>
-        <h6 className='text-[#767676] font-normal text-base'>{productPrice}</h6>
+    <Link to={link} className={`${className} font-dm relative overflow-hidden group transition-transform duration-300 ease-in-out transform hover:scale-105`}>
+      <Img src={productImg} className="w-[370px] h-[360px]" alt={productName} />
+      <Flex className="justify-between mt-5">
+        <h6 className="text-[#262626] font-bold text-xl">{productName}</h6>
+        <h6 className="text-[#767676] font-normal text-base">{productPrice}</h6>
       </Flex>
-      {
-        showBadge && (
-          <h5 className='absolute bg-black py-2 px-8 top-7 left-7 text-white'>{badgeName}</h5>
-        )
-      }
-      <div className='absolute bottom-[55px] bg-red-400 p-8 transition-all scale-0'>
-        <Flex className={"items-center justify-end gap-4"}>
+      {showBadge && (
+        <h5 className="absolute bg-black py-2 px-4 top-7 left-7 text-white">{badgeName}</h5>
+      )}
+      <div className="absolute bottom-[55px] bg-red-400 p-4 transition-all scale-0 group-hover:scale-100">
+        <Flex className="items-center justify-end gap-4">
           <p>Add to Wish List</p>
           <FaHeart />
         </Flex>
-        <Flex className={"items-center justify-end gap-4 mt-6"}>
+        <Flex className="items-center justify-end gap-4 mt-3">
           <p>Compare</p>
           <RiLoopLeftFill />
         </Flex>
-        <Flex className={"items-center justify-end gap-4 mt-6"}>
+        <Flex className="items-center justify-end gap-4 mt-3">
           <p>Add to Cart</p>
           <FaShoppingCart />
         </Flex>
